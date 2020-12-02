f3cb4269905dffe744807d166e93a4d2dbe7ec82
altbdoor <lancersupraskyline@gmail.com>
2020-12-02 23:09:57 +0800

    update packages as of 2020-12-02

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const devMode = process.env.NODE_ENV !== 'production';
-
-module.exports = {
+// @ts-check
+module.exports = /** @type { import('webpack').Configuration } */ {
     entry: './src/main.ts',
     module: {
         rules: [
@@ -22,10 +21,7 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: { hmr: devMode },
-                    },
+                    MiniCssExtractPlugin.loader,
                     'css-loader',
                 ],
             },
@@ -51,7 +47,7 @@ module.exports = {
         extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
-        filename: '[name]-[contenthash].js',
+        filename: '[name]-[hash].js',
         path: path.resolve(__dirname, 'dist'),
         chunkFilename: '[name]-[contenthash].js',
     },
